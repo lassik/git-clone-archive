0aa64f2a4223b18e7513a6a4ad1d28e3752fe698
Lassi Kortela <lassi@lassi.io>
2020-05-06 11:39:30 +0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
+set -x
+git="$(git describe --tags --always --dirty || true)"
+clang -Wall -Wextra -fsanitize=address -o git2tar git2tar.c -D PROGGIT="\"$git\""
