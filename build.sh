61fb8c43a918729ce6d26dee36618b062ea57531
Lassi Kortela <lassi@lassi.io>
2020-05-06 14:01:44 +0300

    Silence any git errors during build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
-git="$(git describe --tags --always --dirty || true)"
+git="$(git describe --tags --always --dirty 2>/dev/null || true)"
 clang \
     -Wall -Wextra -pedantic -std=gnu99 -fsanitize=address \
     -D PROGGIT="\"$git\"" \
