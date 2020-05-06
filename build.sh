bd9199351fe319b29f4b8b8ed2fda0478de6fcb1
Lassi Kortela <lassi@lassi.io>
2020-05-06 13:00:51 +0300

    Reformat

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
 git="$(git describe --tags --always --dirty || true)"
-clang -Wall -Wextra -fsanitize=address -o git2tar git2tar.c -D PROGGIT="\"$git\""
+clang -Wall -Wextra -fsanitize=address -o git2tar git2tar.c \
+    -D PROGGIT="\"$git\""
