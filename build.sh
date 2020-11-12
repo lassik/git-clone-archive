0aca4f6835a028810d25f724dcd102e01e67b05f
Lassi Kortela <lassi@lassi.io>
2020-11-12 13:14:34 +0200

    Use cc instead of clang

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
 git="$(git describe --tags --always --dirty 2>/dev/null || true)"
-clang \
+cc \
     -Wall -Wextra -pedantic -std=gnu99 -fsanitize=address \
     -D PROGGIT="\"$git\"" \
     -o git-clone-tar git-clone-tar.c
